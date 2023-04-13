a0ae63feb41f0d53905d5110f7d515fafd828857
Hajin Park <hajin1819@gmail.com>
2023-04-12 21:40:35 -0700

    Added GTM model and model testing functionality

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
+# TensorFlow is required for Keras to work
+from tensorflow.keras.models import load_model
 import numpy as np
 import cv2
 
 
 def main():
+    # Disable scientific notation for clarity
+    np.set_printoptions(suppress=True)
+
+    # Load the model
+    model = load_model("keras_Model.h5", compile=False)
+
+    # Load the labels
+    class_names = open("labels.txt", "r").readlines()
+
     cap = cv2.VideoCapture(0)
 
     if not cap.isOpened():
@@ -13,16 +24,36 @@ def main():
         # Capture frame-by-frame
         ret, frame = cap.read()
 
+        # Resize the raw image into (224-height,224-width) pixels
+        frame = cv2.resize(frame, (224, 224), interpolation=cv2.INTER_AREA)
         # if frame is read correctly ret is True
         if not ret:
             print("Can't receive frame (stream end?). Exiting ...")
             break
 
         # Our operations on the frame come here
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        color = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
         # Display the resulting frame
-        cv2.imshow('frame', gray)
+        cv2.imshow('frame', color)
+
+        # Make the image a numpy array and reshape it to the models input shape.
+        image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
+
+        # Normalize the image array
+        image = (image / 127.5) - 1
+
+        # Predicts the model
+        prediction = model.predict(image)
+        index = np.argmax(prediction)
+        class_name = class_names[index]
+        confidence_score = prediction[0][index]
+
+        # Print prediction and confidence score
+        print("Class:", class_name[2:], end="")
+        print("Confidence Score:", str(
+            np.round(confidence_score * 100))[:-2], "%")
+
         if cv2.waitKey(1) == ord('q'):
             break
 
