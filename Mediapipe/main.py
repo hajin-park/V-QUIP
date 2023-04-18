6199166ada60a0300581434a59b9acada79436d5
Hajin Park <hajin1819@gmail.com>
2023-04-18 03:18:27 -0700

    Added Handlandmarker visualization

--- Mediapipe/main.py
+++ Mediapipe/main.py
@@ -1,8 +1,99 @@
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+from mediapipe import solutions
+from mediapipe.framework.formats import landmark_pb2
+import numpy as np
 import cv2
 
 
+MARGIN = 10  # pixels
+FONT_SIZE = 1
+FONT_THICKNESS = 1
+HANDEDNESS_TEXT_COLOR = (88, 205, 54)  # vibrant green
+
+BaseOptions = python.BaseOptions
+HandLandmarker = vision.HandLandmarker
+HandLandmarkerOptions = vision.HandLandmarkerOptions
+HandLandmarkerResult = vision.HandLandmarkerResult
+VisionRunningMode = vision.RunningMode
+model_path = "MediaPipe/models/hand_landmarker.task"
+
+
+def draw_landmarks_on_image(rgb_image, detection_result):
+    hand_landmarks_list = detection_result.hand_landmarks
+    handedness_list = detection_result.handedness
+    annotated_image = np.copy(rgb_image)
+
+    # Loop through the detected hands to visualize.
+    for idx in range(len(hand_landmarks_list)):
+        hand_landmarks = hand_landmarks_list[idx]
+        handedness = handedness_list[idx]
+
+        # Draw the hand landmarks.
+        hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+        hand_landmarks_proto.landmark.extend([
+            landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks
+        ])
+        solutions.drawing_utils.draw_landmarks(
+            annotated_image,
+            hand_landmarks_proto,
+            solutions.hands.HAND_CONNECTIONS,
+            solutions.drawing_styles.get_default_hand_landmarks_style(),
+            solutions.drawing_styles.get_default_hand_connections_style())
+
+        # Get the top left corner of the detected hand's bounding box.
+        height, width, _ = annotated_image.shape
+        x_coordinates = [landmark.x for landmark in hand_landmarks]
+        y_coordinates = [landmark.y for landmark in hand_landmarks]
+        text_x = int(min(x_coordinates) * width)
+        text_y = int(min(y_coordinates) * height) - MARGIN
+
+        # Draw handedness (left or right hand) on the image.
+        cv2.putText(annotated_image, f"{handedness[0].category_name}",
+                    (text_x, text_y), cv2.FONT_HERSHEY_DUPLEX,
+                    FONT_SIZE, HANDEDNESS_TEXT_COLOR, FONT_THICKNESS, cv2.LINE_AA)
+
+    return annotated_image
+
+
+def display_frame(result: HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
+    #print('hand landmarker result: {}'.format(result))
+    annotated_image = draw_landmarks_on_image(
+        output_image.numpy_view(), result)
+    annotated_image = cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR)
+    cv2.imshow("frame", annotated_image)
+
+
 def main():
-    pass
+    # Create a hand landmarker instance with the image mode:
+    options = HandLandmarkerOptions(
+        base_options=BaseOptions(model_asset_path=model_path),
+        num_hands=2,
+        running_mode=VisionRunningMode.LIVE_STREAM,
+        result_callback=display_frame)
+
+    with HandLandmarker.create_from_options(options) as landmarker:
+        cap = cv2.VideoCapture(0)
+
+        if not cap.isOpened():
+            print("Cannot open camera")
+            exit()
+
+        timestamp = 0
+        while True:
+            timestamp += 1
+            # Capture frame-by-frame
+            ret, frame = cap.read()
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
+            landmarker.detect_async(mp_image, timestamp)
+
+            if cv2.waitKey(1) == ord('q'):
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
