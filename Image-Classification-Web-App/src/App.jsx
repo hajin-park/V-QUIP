c61b9d9d2eebd3830aa7e9c52eb71a6408c82d59
Hajin Park <hajin1819@gmail.com>
2023-07-09 17:10:46 -0700

    Added basic camera display functionality

--- Image-Classification-Web-App/src/App.jsx
+++ Image-Classification-Web-App/src/App.jsx
@@ -1,7 +1,13 @@
+import WebcamCapture from "./components/WebcamCapture.jsx";
 import "./App.css";
 
 function App() {
-    return <></>;
+    return (
+        <div className="min-h-screen">
+            <h1 className="">Webcam</h1>
+            <WebcamCapture />
+        </div>
+    );
 }
 
 export default App;
