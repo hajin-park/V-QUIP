392c17d54b6e45f335d2f2b6f9368f5f3d9f18df
Hajin Park <hajin1819@gmail.com>
2023-07-30 22:48:29 -0700

    Added gesture recognition system to video formats

--- react-flask-app/flask-app/app.py
+++ react-flask-app/flask-app/app.py
@@ -36,7 +36,7 @@ def inference():
                 pose_annotations = results[0].plot()  # .plot() returns an annotated image
                 height, width = image.shape[:2]
 
-                # each loop represents one person (i.e. three people detected, three loops)
+                # each loop represents one person detected (i.e. three people detected, three loops)
                 for i, keypoints in enumerate(
                     results[0].keypoints.cpu().numpy()
                 ):  # .keypoints returns a Tensor matrix of keypoints, use .cpu().numpy() to convert it
@@ -77,13 +77,15 @@ def inference():
 
             #   Process video file
             elif file_extension in video_extensions:
+                best_gestures = {}
+
                 # Prepare OpenCV VideoWriter to create a new output MP4 file
                 file.save("media.mp4")
                 cap = cv2.VideoCapture("media.mp4")
                 frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
                 frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
                 fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-                out = cv2.VideoWriter("output.mp4", fourcc, 30.0, (frame_width, frame_height))
+                out = cv2.VideoWriter("outputs/output.mp4", fourcc, 30.0, (frame_width, frame_height))
 
                 # Annotate the video frame by frame
                 while cap.isOpened():
@@ -94,6 +96,52 @@ def inference():
                     results = model(frame, save=False)
                     results_plotted = results[0].plot()
                     out.write(results_plotted)
+
+                    # each loop represents one person detected (i.e. three people detected, three loops)
+                    for i, keypoints in enumerate(
+                        results[0].keypoints.cpu().numpy()
+                    ):  # .keypoints returns a Tensor matrix of keypoints, use .cpu().numpy() to convert it
+                        # keypoints has 17 objects, one for each landmark
+                        ear_left = keypoints[3]
+                        ear_right = keypoints[4]
+                        shoulder_left = keypoints[5]
+                        shoulder_right = keypoints[6]
+                        wrist_left = keypoints[9]
+                        wrist_right = keypoints[10]
+                        hand_crop = (
+                            ear_left[0] - ear_right[0]
+                        )  # hand crop dimensions are relative to the size of the person's face
+
+                    for j, wrist_data in enumerate([wrist_left, wrist_right]):
+                        x, y, confidence = wrist_data
+                        hand_raised = y < ((shoulder_left[1] + shoulder_right[1]) / 2) + (hand_crop / 2)
+
+                        # process hand if the confidence is high enough and the arm/hand position is "raised" (around or above the shoulder-line)
+                        if confidence > GESTURE_THRESHOLD and hand_raised:
+                            x_upper, y_upper, x_lower, y_lower = gesture_crop_dimensions(
+                                x, y, hand_crop, frame_width, frame_height
+                            )
+                            wrist_cropped = frame.copy()[
+                                y_upper:y_lower,
+                                x_upper:x_lower,
+                            ]
+                            wrist_cropped_rgb = cv2.cvtColor(
+                                wrist_cropped, cv2.COLOR_BGR2RGB
+                            )  # Mediapipe only accepts RGB images, cv2 returns BGR image so we must convert it here
+                            top_gesture, hand_landmarks = recognize_gesture(wrist_cropped_rgb)
+                            if top_gesture != None:
+                                wrist_cropped_rgb = cv2.cvtColor(
+                                    annotate_gesture_and_hand_landmark(wrist_cropped_rgb, top_gesture, hand_landmarks),
+                                    cv2.COLOR_BGR2RGB,
+                                )  # Mediapipe returns a BGR image, convert it back into RGB
+                                current_gesture = best_gestures.setdefault(
+                                    f"person{i}_gesture{j}", [wrist_cropped_rgb, top_gesture.score]
+                                )
+                                if current_gesture[1] < top_gesture.score:
+                                    best_gestures[f"person{i}_gesture{j}"] = [wrist_cropped_rgb, top_gesture.score]
+
+                for key, value in best_gestures.items():
+                    cv2.imwrite(f"outputs/{key}.jpg", value[0])
 
     elif request.method == "GET":
         pass
