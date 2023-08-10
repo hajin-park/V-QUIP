514fb5ba4e2ed5d0c2b56766a47516e9224fb7c7
Jorge Casta <jorgerochin0102@gmail.com>
2023-08-09 22:35:40 -0700

    Graph Added

--- react-flask-app/react-app/src/components/PollingData/PollingData.jsx
+++ react-flask-app/react-app/src/components/PollingData/PollingData.jsx
@@ -1,12 +1,48 @@
-// Examples of how to extract the polling results
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme, VictoryStack} from 'victory';
+
+
 
 const PollingData = ({ pollingMedia }) => {
     const imageURL = "";
     const videoURL = "http://127.0.0.1:5000/videos";
 
-    return (
-        pollingMedia && (
-            <div className="w-full p-8 bg-slate-800 rounded-md text-center text-white">
+    const data2012 = [
+        {quarter: 1, earnings: 13000},
+        {quarter: 2, earnings: 16500},
+        {quarter: 3, earnings: 14250},
+        {quarter: 4, earnings: 19000}
+      ];
+      
+      const data2013 = [
+        {quarter: 1, earnings: 15000},
+        {quarter: 2, earnings: 12500},
+        {quarter: 3, earnings: 19500},
+        {quarter: 4, earnings: 13000}
+      ];
+      
+      const data2014 = [
+        {quarter: 1, earnings: 11500},
+        {quarter: 2, earnings: 13250},
+        {quarter: 3, earnings: 20000},
+        {quarter: 4, earnings: 15500}
+      ];
+      
+      const data2015 = [
+        {quarter: 1, earnings: 18000},
+        {quarter: 2, earnings: 13250},
+        {quarter: 3, earnings: 15000},
+        {quarter: 4, earnings: 12000}
+      ];
+    
+
+
+
+
+
+
+    return ( <>
+        {/* pollingMedia && ( */}
+            {/* <div className="w-full p-8 bg-slate-800 rounded-md text-center text-white">
                 <h1 className="font-black text-2xl">Results</h1>
                 <p># People: {pollingMedia["people_detected"].count}</p>
                 <p># Gestures: {pollingMedia["gestures_detected"].count}</p>
@@ -40,10 +76,65 @@ const PollingData = ({ pollingMedia }) => {
                             />
                         )
                     )}
-                </div>
-            </div>
-        )
+                </div> */}
+
+                
+
+
+
+
+            {/* </div> */}
+
+
+
+        <div className="w-full p-8 bg-slate-800 rounded-md text-center text-white">  
+        <h1 className="font-black text-2xl">Results</h1>
+        
+        <VictoryChart 
+        domainPadding={20}
+        theme={VictoryTheme.material}
+        >
+
+        <VictoryAxis
+        tickValues={[1,2,3,4]}
+        tickFormat={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
+        />
+
+
+        <VictoryAxis
+        dependentAxis
+        tickFormat={(x) => (`$${x / 100}k`)}
+        />
+
+    <VictoryStack>
+      <VictoryBar
+        data={data2012}
+        x="quarter"
+        y="earnings"
+      />
+      <VictoryBar
+        data={data2013}
+        x="quarter"
+        y="earnings"
+      />
+      <VictoryBar
+        data={data2014}
+        x="quarter"
+        y="earnings"
+      />
+      <VictoryBar
+        data={data2015}
+        x="quarter"
+        y="earnings"
+      />
+    </VictoryStack>
+    </VictoryChart>
+
+      
+      </div>
+      </>
     );
+ 
 };
 
 export default PollingData;
